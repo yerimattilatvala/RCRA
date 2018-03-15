21fe89a91a7225792eed87c61b1a669000cf80c0
Unknown <yeraymendez07@gmail.com>
2018-03-15 15:28:21 +0100

    version2.0

--- PCabalar/functions.py
+++ PCabalar/functions.py
@@ -2,20 +2,11 @@ import numpy as np
 import sys 
 import subprocess
 
+variableNumber = 0
 Count = 0
 Visited = []
 Variables = []
 CurrentVariables = []
-
-def mappingPos(row,col,colsMatrix):
-    atom = (row*colsMatrix) + (col+1)
-    return atom
-
-def posCoord(n,cols):
-    for x in range(cols-1):
-        for y in range(cols):
-            if n == mappingPos(x,y,cols):
-                return x,y 
 
 def formatElements(file):
     n = int(file.readline())    # lee el numero de elementos que deben de ser distintos si no puede formatearlo devuelve un ValueError que es capturado
@@ -30,12 +21,13 @@ def formatElements(file):
         dominosaMatrix = np.array(lista).reshape(n,n+1)
         return dominosaMatrix
 
-def isVariableExists(x):
+def isVariableExists(x1,y1,x2,y2):
     exists = False
-    if x in Variables:
-        exists = True
+    for x in Variables:
+        if (x1,y1) in x and (x2,y2) in x:
+            exists = True
     return exists
-    
+
 def isElementVisited(n1,n2):
     visited = True
     if ((n1,n2) not in Visited) or ((n2,n1) not in Visited):
@@ -47,32 +39,31 @@ def insertElement(n1,n2):
     Visited.append((n2,n1))
 
 def searchHorizontalElements(n1,n2,matrix):
-        rowsMatrix = matrix.shape[0]
-        colsMatrix = matrix.shape[1]
-        for x in range(rowsMatrix):
-            for y in range(colsMatrix):
-                if y != (colsMatrix-1):
-                    if ((matrix[x,y],matrix[x,y+1])==(n1,n2)) or ((matrix[x,y],matrix[x,y+1])==(n2,n1)):
-                        x1 = mappingPos(x,y,colsMatrix)
-                        x2 = mappingPos(x,y+1,colsMatrix)
-                        x1x2 = int(str(x1) + str(x2))
-                        if  not isVariableExists(x1x2):
-                            Variables.append(x1x2) 
-                            CurrentVariables.append(x1x2)    
+    global variableNumber
+    rowsMatrix = matrix.shape[0]
+    colsMatrix = matrix.shape[1]
+    for x in range(rowsMatrix):
+        for y in range(colsMatrix):
+            if y != (colsMatrix-1):
+                if ((matrix[x,y],matrix[x,y+1])==(n1,n2)) or ((matrix[x,y],matrix[x,y+1])==(n2,n1)):
+                    if not isVariableExists(x,y,x,y+1):
+                        variableNumber += 1
+                        Variables.append(((x,y),(x,y+1),variableNumber))
+                        CurrentVariables.append(variableNumber)
+
 
 def searchVerticalElements(n1,n2,matrix):
-        rowsMatrix = matrix.shape[0]
-        colsMatrix = matrix.shape[1]
-        for y in range(colsMatrix):
-            for x in range(rowsMatrix):
-                if x!=rowsMatrix-1:
-                    if ((matrix[x,y],matrix[x+1,y])==(n1,n2)) or ((matrix[x,y],matrix[x+1,y])==(n2,n1)):
-                        x1 = mappingPos(x,y,colsMatrix)
-                        x2 = mappingPos(x+1,y,colsMatrix)
-                        x1x2 = int(str(x1) + str(x2))
-                        if  not isVariableExists(x1x2):
-                                Variables.append(x1x2) 
-                                CurrentVariables.append(x1x2) 
+    global variableNumber
+    rowsMatrix = matrix.shape[0]
+    colsMatrix = matrix.shape[1]
+    for y in range(colsMatrix):
+        for x in range(rowsMatrix):
+            if x!=rowsMatrix-1:
+                if ((matrix[x,y],matrix[x+1,y])==(n1,n2)) or ((matrix[x,y],matrix[x+1,y])==(n2,n1)):
+                    if not isVariableExists(x,y,x+1,y):
+                        variableNumber += 1
+                        Variables.append(((x,y),(x+1,y),variableNumber))
+                        CurrentVariables.append(variableNumber)        
 
 def writeNegativeVariables(file,x):
     global Count
@@ -96,37 +87,25 @@ def writeVariables(n1,n2,file):
 def obtainCrossVariables(x):
     global Count
     line = ""
-    number = str(x)
-    #line +="-"+number+" "
-    l = len(number)
-    n1 = int(number[:int(l/2)])
-    n2 = int(number[int(l/2):])
     for t in Variables:
-        if t!=x:
-            numbert = str(t)
-            lt = len(numbert)
-            n3 = int(numbert[:int(lt/2)]) 
-            n4 = int(numbert[int(lt/2):]) 
-            if n1 == n3 or n2 == n3 or n2 == n4 or n1 == n4:
-                line+="-"+number+" -"+numbert+" 0\n"
+        if t[2]!=x[2]:
+            if x[0] == t[0] or x[1] == t[0] or x[0] == t[1] or x[1] == t[1]:
+                line+="-"+str(x[2])+" -"+str(t[2])+" 0\n"
                 Count +=1
     #line+="0\n"
     return line
 
 def noCross(file):
-    global Count
-    clauses = 0
     f= open("satfile.cnf","w")
-    Variables.sort()
     f.write("c Variables : \n")
     file.write("c No hay solapamiento entre fichas. \n")
     f.write("c ")
     for x in Variables:
-        f.write(str(x)+" ")
+        f.write(str(x[2])+" ")
         file.write(obtainCrossVariables(x))
     file.close()
     f.write("\n")
-    f.write("p cnf "+str(max(Variables))+" "+str(Count)+" \n")
+    f.write("p cnf "+str(variableNumber)+" "+str(Count)+" \n")
     f1 = open("clauses.txt","r")
     for lineReaded in f1:
         f.write(lineReaded)
@@ -158,19 +137,21 @@ def isSatisfiable(file):
         print('Going out of the program...')
         sys.exit()
 
+def returnCoords(x):
+    for v in Variables:
+        if x == v[2]:
+            return(v[0],v[1])
+
 def writeSolution(list,rows,cols):
     charar = np.chararray((rows,cols))
     for x in list:
-        numbert = str(x)
-        lt = len(numbert)
-        n1 = int(numbert[:int(lt/2)]) 
-        n2 = int(numbert[int(lt/2):])
-        if n2 == n1+1:
-            charar[posCoord(n1,cols)]='>'
-            charar[posCoord(n2,cols)]='<'
-        else:
-            charar[posCoord(n1,cols)]='v'
-            charar[posCoord(n2,cols)]='^'
+        c1,c2 = returnCoords(x)
+        if c2[1] == c1[1]+1:
+            charar[c1]='>'
+            charar[c2]='<'
+        elif c2[0] == c1[0]+1:
+            charar[c1]='v'
+            charar[c2]='^'
     line = ''
     i = 0
     charar.astype(str)
@@ -187,6 +168,7 @@ def writeSolution(list,rows,cols):
 
         
 def solveDominosa(matrix):
+    global variableNumber
     rows = matrix.shape[0]
     cols = matrix.shape[1]
     f= open("clauses.txt","w")
@@ -202,32 +184,27 @@ def solveDominosa(matrix):
             if n1 is not None and n2 is not None:
                 if not isElementVisited(n1,n2):
                     insertElement(n1,n2)
-                    x1 = mappingPos(x,y,cols)
-                    x2 = mappingPos(x,y+1,cols)
-                    x1x2 = int(str(x1) + str(x2))
-                    if  not isVariableExists(x1x2):
-                        Variables.append(x1x2)
-                        CurrentVariables.append(x1x2)
-                    searchHorizontalElements(n1,n2,matrix)
-                    searchVerticalElements(n1,n2,matrix)
-                    #method que escribe convinacion variables
-                    writeVariables(n1,n2,f)
-                    del CurrentVariables[:]
+                    if not isVariableExists(x,y,x,y+1):
+                        variableNumber +=1
+                        Variables.append(((x,y),(x,y+1),variableNumber)) 
+                        CurrentVariables.append(variableNumber)  
+                        searchHorizontalElements(n1,n2,matrix)
+                        searchVerticalElements(n1,n2,matrix)
+                        #metodo que escribe combinacion variables
+                        writeVariables(n1,n2,f)
+                        del CurrentVariables[:]
             if n1 is not None and n3 is not None:
                 if not isElementVisited(n1,n3):
                     insertElement(n1,n3)
-                    x1 = mappingPos(x,y,cols)
-                    x2 = mappingPos(x+1,y,cols)
-                    x1x2 = int(str(x1) + str(x2))
-                    if  not isVariableExists(x1x2):
-                        Variables.append(x1x2)
-                        CurrentVariables.append(x1x2)
-                    searchHorizontalElements(n1,n3,matrix)
-                    searchVerticalElements(n1,n3,matrix)
-                    #method que escribe convinacion variables
-                    writeVariables(n1,n3,f)
-                    del CurrentVariables[:]
-            #return 0
+                    if not isVariableExists(x,y,x+1,y):
+                        variableNumber +=1
+                        Variables.append(((x,y),(x+1,y),variableNumber)) 
+                        CurrentVariables.append(variableNumber)
+                        searchHorizontalElements(n1,n3,matrix)
+                        searchVerticalElements(n1,n3,matrix)
+                        #metodo que escribe combinacion variables
+                        writeVariables(n1,n3,f)
+                        del CurrentVariables[:]
     noCross(f)
     result = open('result.txt','w')
     content = subprocess.run(["clasp", "satfile.cnf"], stdout=subprocess.PIPE)
